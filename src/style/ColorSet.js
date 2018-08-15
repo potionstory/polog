3687814d2a2eedc86decec91062e77e312ceee01
Potion Story <potionstory@gmail.com>
2018-08-16 01:20:00 +0900

    Header 컴포넌트 ColorSet 수정

--- src/style/ColorSet.js
+++ src/style/ColorSet.js
@@ -5,11 +5,11 @@ export const ColorSet = {
   darkGray: "#2d2e30",
   gray: "#515254",
   lightGray: "#7f8082",
-  red: "#f44336",
-  orange: "#ff9800",
-  yellow: "#ffd600",
-  green: "#8bc34a",
-  blue: "#2196f3",
-  indigo: "#3f51b5",
-  purple: "#9c27b0"
+  red: "#B71C1C",
+  orange: "#EF6C00",
+  yellow: "#F9A825",
+  green: "#558B2F",
+  blue: "#1565C0",
+  indigo: "#283593",
+  purple: "#6A1B9A"
 };
